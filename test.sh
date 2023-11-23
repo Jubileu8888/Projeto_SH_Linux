f659ca9a485da907e7b4fce0d1ce17e3642af6e3
Jubileu8888 <127453392+Jubileu8888@users.noreply.github.com>
2023-11-22 21:05:53 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 echo "------------------------------------------------------------"
 echo "---------------------Digite uma opção-----------------------"
 echo "------------------------------------------------------------"
@@ -9,7 +7,6 @@ echo "------------------------------------------------------------"
 # opções
 echo "1-Indentificar quais computadores estão ligados."
 echo "2-Desligar os computadores da rede."
-echo "3-Bloquear internet"
 echo "Digite uma opção: "
 read op
 clear
@@ -22,8 +19,9 @@ case $op in
         echo "------------------------------------------------------------"
         echo "-----------------------Computadores-------------------------"
         echo "------------------------------------------------------------"
-        arp-scan --localnet | awk '/^[0-9]+.[0-9]+.[0-9]+.[0-9]+/ {print $1}' > ips.txt
-        ifconfig | grep -A1 "$(ip r | grep default | awk '{print $5}')" | awk '/netmask/ {print $4}' > mascara.txt
+        # "awk" é usado para extrair informações específicas de saidas de comandos
+        sudo arp-scan --localnet | awk '/^[0-9]+.[0-9]+.[0-9]+.[0-9]+/ {print $1}' > ips.txt # aqui foi usado awk para extrair os ips do comando arp-scan --localnet
+        ifconfig | grep -A1 "$(ip r | grep default | awk '{print $5}')" | awk '/netmask/ {print $4}' > mascara.txt # awk foi ultilizado para extrair a mascara ip pelo comando ifconfig
         echo "Lista de todos os ips disponiveis da sua rede local."
         cat ips.txt
 
@@ -31,16 +29,26 @@ case $op in
         echo "-------------------Verificação do ping----------------------"
         echo "------------------------------------------------------------"
         # verificando se os ips que o comando deu estão respondendo o ping.
+
+        #Estrutura comumente usada para ler linhas de um arquivo ou da saída de um comando, uma linha por vez.
+        #O IFS é responsavel em que a linha lida seja tratada como uma unica variavel string completa.
+        #O read -r ip, lê uma linha do input e atribui essa linha a variavel ip, e o -r desativa a interpretação de barras invertidas e de caracteres especiais.
         while IFS= read -r ip; do
 
         ping -c 2 "$ip" &>/dev/null
         
+        # verifica se o comando anterior foi executado com sucesso. Se o status de saída for zero, o bloco de comandos dentro do then será executado.
+        # A variavel $? é uma variável especial em shell que contém o status de saída do último comando executado o valor 0 significa que o comando foi executado com sucesso.
+        # -eq significa "=" "igual"
         if [ $? -eq 0 ]; then
         echo "O IP $ip está respondendo o ping."
         else
         echo "O IP $ip não está respondendo o ping."
         fi
     done < "ips.txt"
+    sleep 5
+    clear
+    bash ./test.sh
 ;;
 # fim op1
 # comeco op2
@@ -50,79 +58,41 @@ case $op in
     echo "------------------------------------------------------------"
 
 
-
+# Coletando o arquivo txt que possui os ips ligados na rede.
 ip_file="ips.txt"
 
-
+# Coletando user do SSH destino.
 ssh_user="admin"
 
-
+#Coletando senha do ssh destino
 ssh_password="admin"
 
-
+#Estrutura comumente usada para ler linhas de um arquivo ou da saída de um comando, uma linha por vez.
+#O IFS é responsavel em que a linha lida seja tratada como uma unica variavel string completa.
+#O read -r ip, lê uma linha do input e atribui essa linha a variavel ip, e o -r desativa a interpretação de barras invertidas e de caracteres especiais.
 while IFS= read -r ip; do
 
     ssh -p "$ssh_password"
+    # comando remoto via SSH para desligar o computador remoto.
+    # O -n Redireciona a entrada padrão do comando remoto a partir de /dev/null. Isso é útil quando você não precisa de interação interativa com o shell remoto.
+    # Força um pseudo-tty a ser alocado. Isso é usado para interagir com comandos interativos.
+    # "echo '$ssh_password' | sudo -S nohup init 0": Este é o comando que está sendo executado no host remoto.
+    # nohup é usado para garantir que o comando continue a ser executado mesmo se a conexão SSH for encerrada.
     ssh -n -t "$ssh_user@$ip" "echo '$ssh_password' | sudo -S nohup init 0"
     
+    # verifica se o comando anterior foi executado com sucesso. Se o status de saída for zero, o bloco de comandos dentro do then será executado.
+    # A variavel $? é uma variável especial em shell que contém o status de saída do último comando executado o valor 0 significa que o comando foi executado com sucesso.
+    # -eq significa "=" "igual"
     if [ $? -eq 0 ]; then
         echo "Desligamento bem-sucedido para o IP $ip."
     else
         echo "Falha ao desligar o IP $ip."
     fi
 done < "$ip_file"
-
+sleep 3
+clear
+bash ./test.sh
     ;;
 # fimop2
-# comeco op3
-3)
-    ip_file="ips.txt"
-
-    network_interface="eth0"
-
-    while IFS= read -r ip; do
-    iptables -A OUTPUT -o "$network_interface" -d "$ip" -j DROP
-    done < "$ip_file"
-
-    ;;
-# fimop3
-# comeco op4
-4)
-    # Site permitido
-    allowed_site="youtube.com"
-
-    # Interface de rede
-    network_interface="eth0"
-
-    iptables -F
-
-    iptables -P OUTPUT DROP
-
-    iptables -A OUTPUT -o "$network_interface" -d "$allowed_site" -j ACCEPT
-    ;;
-  5)
-    echo "------------------------------------------------------------"
-    echo "----------------------Regras Firewall-----------------------"
-    echo "------------------------------------------------------------"
-    echo
-    echo
-    iptables -L
-    ;;
-  6)
-    echo "------------------------------------------------------------"
-    echo "------------------Desfazer regra Firewall-------------------"
-    echo "------------------------------------------------------------"
-    echo
-    echo
-    echo "Digite o número do tipo da regra que você quer desfazer"
-    echo "1-INPUT"
-    echo "2-OUTPUT"
-    echo "3-FORWARD"
-    echo "4-PREROUTING"
-    echo "5-POSTROUTING"
-    read -p "Digite uma opção:" op2
-      
-    ;;
-
 esac
 #fim do case
